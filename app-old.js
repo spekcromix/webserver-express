c44e389bdc4419964c6d7eccf57f31e8ceebe925
Luis Rodolfo <rodolfolarae@gmail.com>
2018-07-01 20:49:35 -0500

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+http.createServer( (req,res) => {
+	res.writeHead(200, {'Content-Type': 'application/json' })
+	
+	let salida = {
+		nombre: 'Rodolfo',
+		edad: 33,
+		url: req.url
+	}
+	//res.write('Hola Mundo');
+	res.write( JSON.stringify(salida) )
+	res.end();
+})
+.listen(8080);
+
+console.log('Escuchando el puerto 8080');
